1823d38e7a39190fa7c4f3280754021ae4630c8d
dev-javascript <javascript.code.dev@gmail.com>
2022-05-25 08:13:37 +0430

    update index.js : output component should not be memomized and updateTemplate method should be executed once in each render

--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
-import React, { useRef } from "react";
-import Ctx from "./ctx.js";
+import React, {useRef} from 'react';
+import Ctx from './ctx.js';
 window.React = window.React || React;
 function StringToReactComponent(props) {
   const ref = useRef(null),
     template = props.children;
   if (!ref.current) {
-    ref.current = new Ctx(template);
+    ref.current = new Ctx();
   }
-  const GeneratedComponent = ref.current
-    .updateTemplate(template)
-    .getComponent();
+  const GeneratedComponent = ref.current.updateTemplate(template).getComponent();
   return <GeneratedComponent {...props} />;
 }
-export default React.memo(StringToReactComponent);
+export default StringToReactComponent;
