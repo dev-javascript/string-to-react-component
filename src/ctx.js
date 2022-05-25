f2886b67e561df016af070724e2c19b1a6017217
dev-javascript <javascript.code.dev@gmail.com>
2022-05-25 13:54:59 +0430

    update ctx.js and add ctx.test.js

--- src/ctx.js
+++ src/ctx.js
@@ -2,13 +2,10 @@ class Ctx {
   constructor() {
     this._temp = '';
     this._parentTemp = `"use strict";return @temp;`;
-    this._defaultCom = null;
     this._com = null;
     if (!(window.hasOwnProperty('Babel') && typeof window.Babel === 'object')) {
-      throw new Error(`
-      string-to-react-component package needs @babel/standalone for working correctly.
-      you should load @babel/standalone in the browser.
-      `);
+      throw new Error(`string-to-react-component package needs @babel/standalone for working correctly.
+      you should load @babel/standalone in the browser.`);
     }
     this._b = window.Babel;
     this._babelpresets = ['react'];
@@ -19,19 +16,24 @@ class Ctx {
     }).code;
   }
   _generateCom() {
-    this._com = this._temp ? Function(this._parentTemp.replace('@temp', this._transpile()))() : this._defaultCom;
+    this._com = Function(this._parentTemp.replace('@temp', this._transpile()))();
+    this._validateCodeInsideTheTemp();
+  }
+  _validateCodeInsideTheTemp() {
+    if (typeof this._com !== 'function') {
+      throw new Error(`code inside the passed string into string-to-react-component, should be a function`);
+    }
   }
   _validateTemplate(temp) {
     if (typeof temp !== 'string') {
-      throw `passed child into string-to-react-component element should b a string`;
+      throw new Error(`passed child into string-to-react-component element should b a string`);
     }
     if (temp === '') {
-      throw `passed string into string-to-react-component element can not be empty`;
+      throw new Error(`passed string into string-to-react-component element can not be empty`);
     }
   }
   updateTemplate(template) {
     this._validateTemplate(template);
-    template = template || '';
     if (template !== this._temp) {
       this._temp = template;
       this._generateCom();
---
+++ src/ctx.test.js
@@ -0,0 +1,95 @@
+import Ctx from './ctx.js';
+import {TestScheduler} from 'jest';
+beforeEach(() => {
+  window.Babel = {};
+});
+afterEach(() => {
+  delete window.Babel;
+});
+describe('constructor :', () => {
+  test('it should work correctly without errors', () => {
+    new Ctx();
+    expect(1).toBe(1);
+  });
+  test('it should throw an error when Babel global variable is not existed', () => {
+    delete window.Babel;
+    expect.assertions(1);
+    try {
+      new Ctx();
+    } catch (er) {
+      expect(er.message).toBe(`string-to-react-component package needs @babel/standalone for working correctly.
+      you should load @babel/standalone in the browser.`);
+    }
+  });
+  test('check _parentTemp property', () => {
+    const ins = new Ctx();
+    expect(ins._parentTemp).toBe(`"use strict";return @temp;`);
+  });
+  test('check _babelpresets property', () => {
+    const ins = new Ctx();
+    expect(ins._babelpresets).toEqual(['react']);
+  });
+});
+describe('methods : ', () => {
+  test('_validateTemplate method ', () => {
+    expect.assertions(3);
+    const ins = new Ctx();
+    try {
+      ins._validateTemplate({});
+    } catch (er) {
+      expect(er.message).toBe('passed child into string-to-react-component element should b a string');
+    }
+    try {
+      ins._validateTemplate();
+    } catch (er) {
+      expect(er.message).toBe('passed child into string-to-react-component element should b a string');
+    }
+    try {
+      ins._validateTemplate('');
+    } catch (er) {
+      expect(er.message).toBe('passed string into string-to-react-component element can not be empty');
+    }
+  });
+  test('_validateCodeInsideTheTemp method', () => {
+    expect.assertions(3);
+    const ins = new Ctx();
+    try {
+      ins._com = () => {};
+      ins._validateCodeInsideTheTemp();
+      expect(1).toBe(1);
+    } catch (er) {}
+    try {
+      class c {}
+      ins._com = c;
+      ins._validateCodeInsideTheTemp();
+      expect(1).toBe(1);
+    } catch (er) {}
+    try {
+      ins._com = {};
+      ins._validateCodeInsideTheTemp();
+    } catch (er) {
+      expect(er.message).toBe('code inside the passed string into string-to-react-component, should be a function');
+    }
+  });
+  test('_generateCom method', () => {
+    const ins = new Ctx();
+    ins._transpile = () => '() => {}';
+    ins._validateCodeInsideTheTemp = jest.fn(() => {});
+    ins._generateCom();
+    expect(ins._validateCodeInsideTheTemp.mock.calls.length).toBe(1);
+  });
+  test('updateTemplate method', () => {
+    const ins = new Ctx();
+    ins._validateTemplate = jest.fn();
+    ins._generateCom = jest.fn();
+    let temp = '()=>3';
+    ins._temp = '()=>3';
+    ins.updateTemplate(temp);
+    expect(ins._validateTemplate.mock.calls.length).toBe(1);
+    expect(ins._validateTemplate.mock.calls[0][0]).toBe(temp);
+    expect(ins._generateCom.mock.calls.length).toBe(0);
+    ins._temp = '';
+    ins.updateTemplate(temp);
+    expect(ins._generateCom.mock.calls.length).toBe(1);
+  });
+});
