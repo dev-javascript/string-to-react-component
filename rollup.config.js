dadff26c2278ab1d71bb954393302f5e0693a451
dev-javascript <javascript.code.dev@gmail.com>
2024-02-06 19:41:53 +0330

    update rollup config : change lib name

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,11 @@
-import { terser } from 'rollup-plugin-terser';
+import {terser} from 'rollup-plugin-terser';
 import commonjs from '@rollup/plugin-commonjs';
 import nodeResolve from '@rollup/plugin-node-resolve';
+const pkg = require('./package.json');
+const name = pkg.name
+  .split('-')
+  .map((s) => s.charAt(0).toUpperCase() + s.slice(1))
+  .join('');
 const requirePolyfills = process.env.INCLUDE_POLYFILLS;
 export default {
   input: requirePolyfills ? 'lib/esm-including-polyfills/index.js' : 'lib/esm/index.js',
@@ -9,15 +14,15 @@ export default {
       ? 'dist/stringToReactComponent.including-polyfills.umd.min.js'
       : 'dist/stringToReactComponent.umd.min.js',
     format: 'umd',
-    name: 'stringToReactComponent',
+    name,
     globals: {
       'react-dom': 'ReactDOM',
       react: 'React',
-      '@babel/standalone': 'Babel'
+      '@babel/standalone': 'Babel',
     },
     sourcemap: true,
   },
-  plugins: [terser(), commonjs(), nodeResolve({ preferBuiltins: false })],
+  plugins: [terser(), commonjs(), nodeResolve({preferBuiltins: false})],
   external: function (id) {
     return /prop-types$|react$|\@babel\/standalone$|react-dom$|.test.js$|.js.snap$|.css$/g.test(id);
   },
