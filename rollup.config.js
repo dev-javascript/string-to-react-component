3b25ac6f9a34b1793aa8bf11452c9e5bd4e44c07
dev-javascript <javascript.code.dev@gmail.com>
2024-02-13 20:57:40 +0330

    use esbuild

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,8 @@
-import {terser} from 'rollup-plugin-terser';
+//import {terser} from 'rollup-plugin-terser';
+import esbuild from 'rollup-plugin-esbuild';
 import commonjs from '@rollup/plugin-commonjs';
 import nodeResolve from '@rollup/plugin-node-resolve';
+//import {dts} from 'rollup-plugin-dts';
 const pkg = require('./package.json');
 const name = pkg.name
   .split('-')
@@ -22,15 +24,44 @@ const Config = ({en, inputPath = '', outputFile = 'stringToReactComponent', pf =
         sourcemap: true,
       },
       plugins: (function () {
-        const _plugins = [nodeResolve({preferBuiltins: false}), commonjs()];
-        if (en === 'prod') {
-          _plugins.push(terser());
-        }
+        const _plugins = [
+          //nodeResolve({preferBuiltins: false}),
+          //commonjs(),
+          //dts(),
+          esbuild({
+            minify: en === 'prod',
+            // All options are optional
+            include: /\.[jt]sx?$/, // default, inferred from `loaders` option
+            exclude: /node_modules/, // default
+            sourceMap: true, // default
+            target: 'es2017', // default, or 'es20XX', 'esnext'
+            jsx: 'transform', // default, or 'preserve'
+            jsxFactory: 'React.createElement',
+            jsxFragment: 'React.Fragment',
+            // Like @rollup/plugin-replace
+            define: {
+              __VERSION__: '"x.y.z"',
+            },
+            tsconfig: 'tsconfig.json', // default
+            // Add extra loaders
+            loaders: {
+              // Add .json files support
+              // require @rollup/plugin-commonjs
+              '.json': 'json',
+              // Enable JSX in .js files too
+              '.js': 'jsx',
+            },
+          }),
+        ];
+        // if (en === 'prod') {
+        //   _plugins.push(terser());
+        // }
         return _plugins;
       })(),
       external: function (id) {
         return /prop-types$|react$|\@babel\/standalone$|react-dom$|.test.js$|.js.snap$|.css$/g.test(id);
       },
+      banner: '/** MIT licence */',
     };
   },
   ConfigFactory = (op) => [
